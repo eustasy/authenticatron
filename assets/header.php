2794b6a8576ad72e0913d9b8d9f48b060e9b6c4e
Lewis Goddard <lewisgoddard@eustasy.org>
2016-01-26 23:33:50 +0000

    Re-order navigation, add source to navigation.

--- assets/header.php
+++ assets/header.php
@@ -18,8 +18,9 @@
 		<h1>Authenticatron</h1>
 		<p>A simple, procedural PHP script to create Google Authenticator secrets and corresponding QR codes,<br>
 		then verify the entered response over a given time variance.<br>
-		<a href="server.php">server</a> &emsp;
 		<a href="documentation.php">documentation</a> &emsp;
+		<a href="server.php">server</a> &emsp;
+		<a href="https://github.com/eustasy/authenticatron">source</a> &emsp;
 		<a href="https://www.codacy.com/public/eustasy/authenticatron"><img src="https://api.codacy.com/project/badge/670334725e9240d1beddb0b34f0d8c3c"/></a> &emsp;
 		<a href="https://codeclimate.com/github/eustasy/authenticatron"><img src="https://codeclimate.com/github/eustasy/authenticatron/badges/gpa.svg" /></a></p>
 	</div>
