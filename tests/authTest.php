0e79b92d33f8987f8adefa671004c004a5c08963
Lewis Goddard <lewis.goddard@eustasy.org>
2024-03-10 05:20:08 +0000

    Update authTest.php

--- tests/authTest.php
+++ tests/authTest.php
@@ -11,24 +11,21 @@ final class AuthTest extends TestCase
     // Test that the secret is 16 characters long
     public function testSecretLength(): void
     {
-        $auth = new Authenticatron();
-        $secret = $auth->makeSecret();
+        $secret = Authenticatron::makeSecret();
         $this->assertEquals(16, strlen($secret));
     }
 
     // Test that the secret is a string
     public function testSecretType(): void
     {
-        $auth = new Authenticatron();
-        $result = $auth->makeSecret();
+        $result = Authenticatron::makeSecret();
         $this->assertIsString($result);
     }
 
     // Test that the secret is a string
     public function testSecretChars(): void
     {
-        $auth = new Authenticatron();
-        $result = $auth->makeSecret();
+        $result = Authenticatron::makeSecret();
         $this->assertMatchesRegularExpression('/^[A-Z2-7]+$/', $result);
     }
 
@@ -36,45 +33,40 @@ final class AuthTest extends TestCase
     // Test that the URL is a specific string
     public function testUrlChars(): void
     {
-        $auth = new Authenticatron();
-        $result = $auth->getUrl('test', 'test');
+        $result = Authenticatron::getUrl('accountName', 'secret', 'issuer');
         $this->assertEquals('otpauth://totp/Example Site: test?secret=test&issuer=Example+Site', $result);
     }
 
     // Test that the qrCode is a string
     public function testQrCodeType(): void
     {
-        $auth = new Authenticatron();
-        $url = $auth->getUrl('test', 'test');
-        $result = $auth->generateQrCode('url');
+        $url = Authenticatron::getUrl('accountName', 'secret', 'issuer');
+        $result = Authenticatron::generateQrCode('url');
         $this->assertIsString($result);
     }
 
     // Test that the current code is a 6 digit number
     public function testCurrentCode(): void
     {
-        $auth = new Authenticatron();
-        $secret = $auth->makeSecret();
-        $result = $auth->getCode($secret);
+        $secret = Authenticatron::makeSecret();
+        $result = Authenticatron::getCode($secret);
         $this->assertEquals(6, strlen($result));
     }
 
     // Test that there are 5 codes in the range
     public function testCodeRange(): void
     {
-        $auth = new Authenticatron();
-        $secret = $auth->makeSecret();
-        $result = $auth->getCodesInRange($secret);
+        $secret = Authenticatron::makeSecret();
+        $result = Authenticatron::getCodesInRange($secret);
         $this->assertCount(5, $result);
     }
 
     // Test that the code is valid
     public function testCodeValid(): void
     {
-        $auth = new Authenticatron();
-        $secret = $auth->makeSecret();
-        $code = $auth->getCode($secret);
-        $result = $auth->checkCode($code, $secret);
+        $secret = Authenticatron::makeSecret();
+        $code = Authenticatron::getCode($secret);
+        $result = Authenticatron::checkCode($code, $secret);
         $this->assertTrue($result);
     }
 }
