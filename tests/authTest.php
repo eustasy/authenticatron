97a0a3df3f6553bf3ea7011eaa6e4600127de31e
Lewis Goddard <lewis.goddard@eustasy.org>
2024-03-14 03:04:14 +0000

    Update authTest.php

--- tests/authTest.php
+++ tests/authTest.php
@@ -1,7 +1,7 @@
 <?php
 declare(strict_types=1);
 
-require_once __DIR__ . '/../authenticatron.php';
+require_once __DIR__ . '/vendor/autoload.php';
 use eustasy\Authenticatron;
 
 use PHPUnit\Framework\TestCase;
