f646abdb2111afd524e0fe9c69e51424d9eb5e4b
Lewis Goddard <lewis.goddard@eustasy.org>
2024-03-15 04:13:27 +0000

    Update index.php

--- index.php
+++ index.php
@@ -165,34 +165,6 @@ if (
 	</div>
 </div>
 
-<div class="break clear"></div>
-<hr>
-<div class="break clear"></div>
+<?php
 
-<div class="left">
-	<img alt="lifefloat" src="assets/google_help-128.png">
-</div>
-<div class="right">
-	<h3 class="color-flatui-pomegranate">Further Reading</h3>
-	<p>Visit our <a href="documentation.php">documentation</a> for a more thorough description of the options and functions available to you.</p>
-	<p>Take a look at the <a href="documentation.php#glossary">glossary</a> if there are any terms you don't understand.</p>
-	<p>The <a href="server.php">server</a> page can be used if this script is installed on your server to check for requirements.</p>
-	<p>If you're ready to rock, check out the <a href="https://github.com/eustasy/authenticatron">source</a>!</p>
-</div>
-<div class="break clear"></div>
-<div class="left">
-	<img src="assets/Open_Source_Initiative_keyhole.svg" alt="Open Source Initiative Keyhole">
-</div>
-<div class="right">
-	<p>This work is predominantly MIT licensed. See the <a href="https://github.com/eustasy/authenticatron/blob/mainCENSE.md">LICENSE.md file</a> for more information.</p>
-	<p>
-		<a href="https://codeclimate.com/github/eustasy/authenticatron"><img src="https://codeclimate.com/github/eustasy/authenticatron/badges/gpa.svg"></a> &emsp;
-		<a href="https://github.com/eustasy/authenticatron/actions/workflows/normal.yml"><img src="https://github.com/eustasy/authenticatron/actions/workflows/normal.yml/badge.svg"></a>
-	</p>
-	</p>
-</div>
-
-<div class="clear break"></div>
-</body>
-
-</html>
+include __DIR__ . '/assets/footer.php';
