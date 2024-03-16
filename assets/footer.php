8c0adfe0334860b9747cbd1d6cb7c5d21a2fc23e
Lewis Goddard <lewis.goddard@eustasy.org>
2024-03-16 07:30:17 +0000

    Update footer.php

--- assets/footer.php
+++ assets/footer.php
@@ -10,19 +10,8 @@
 	<p>Visit our <a href="documentation.php">documentation</a> for a more thorough description of the options and functions available to you.</p>
 	<p>Take a look at the <a href="documentation.php#glossary">glossary</a> if there are any terms you don't understand.</p>
 	<p>The <a href="server.php">server</a> page can be used if this script is installed on your server to check for requirements.</p>
-	<p>If you're ready to rock, check out the <a href="https://github.com/eustasy/authenticatron">source</a>!</p>
-</div>
-<div class="break clear"></div>
-<div class="left">
-	<img alt="Open Source Initiative Keyhole Logo" src="assets/Open_Source_Initiative_keyhole.svg">
-</div>
-<div class="right">
 	<p>This work is predominantly MIT licensed. See the <a href="https://github.com/eustasy/authenticatron/blob/mainCENSE.md">LICENSE.md file</a> for more information.</p>
-	<p>
-		<a href="https://codeclimate.com/github/eustasy/authenticatron"><img alt="Code Climate Status Badge" src="https://codeclimate.com/github/eustasy/authenticatron/badges/gpa.svg"></a> &emsp;
-		<a href="https://github.com/eustasy/authenticatron/actions/workflows/normal.yml"><img alt="GitHub Actions Status Badge" src="https://github.com/eustasy/authenticatron/actions/workflows/normal.yml/badge.svg"></a>
-	</p>
-	</p>
+	<p>If you're ready to rock, check out the <a href="https://github.com/eustasy/authenticatron">source</a>!</p>
 </div>
 
 <div class="clear break"></div>
