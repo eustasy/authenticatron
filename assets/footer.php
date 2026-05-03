4d6c447f366d7c44b81eb3af4a9386c5debc270e
Lewis Goddard <lewis.goddard@eustasy.org>
2026-05-03 15:36:12 +0100

    Update footer.php

--- assets/footer.php
+++ assets/footer.php
@@ -10,7 +10,7 @@
 	<p>Visit our <a href="documentation.php">documentation</a> for a more thorough description of the options and functions available to you.</p>
 	<p>Take a look at the <a href="documentation.php#glossary">glossary</a> if there are any terms you don't understand.</p>
 	<p>The <a href="server.php">server</a> page can be used if this script is installed on your server to check for requirements.</p>
-	<p>This work is predominantly MIT licensed. See the <a href="https://github.com/eustasy/authenticatron/blob/mainCENSE.md">LICENSE.md file</a> for more information.</p>
+	<p>This work is predominantly MIT licensed. See the <a href="https://github.com/eustasy/authenticatron/blob/main/LICENSE.md">LICENSE.md file</a> for more information.</p>
 	<p>If you're ready to rock, check out the <a href="https://github.com/eustasy/authenticatron">source</a>!</p>
 </div>
 
